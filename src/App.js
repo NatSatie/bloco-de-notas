b3ddee958ee59396ce84465d4a2cf1fa0bd08876
Natália Satie <naty.odashima@gmail.com>
2021-07-29 20:39:12 -0300

    remove link from font

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react';
 import Cadastro from './components/Cadastro';
 import ListaDeNotas from './components/ListaDeNotas';
-import { NotasProvider } from './context/NotasContext';
+import { Container } from './style/App';
 
 function App() {
   return (
-    <>
-      <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@700&display=swap" rel="stylesheet"></link>
+    <Container>
       <Cadastro/>
       <ListaDeNotas/>
-    </>
+    </Container>
   );
 }
 
